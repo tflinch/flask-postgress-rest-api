20b479b5b9c8316abdec98942926a881af9c3e87
tflinch <tflinch16@gmail.com>
2024-08-14 14:07:35 -0400

    feat: add crud create '/create_pets'

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 from dotenv import load_dotenv
-import os, psycopg2
+import os, psycopg2 , psycopg2.extras
 load_dotenv()
 
 app = Flask(__name__)
@@ -21,10 +21,25 @@ def index():
 @app.route('/pets')
 def pets_index():
     connection = get_db_connection()
-    cursor = connection.cursor()
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
     cursor.execute("SELECT * FROM pets;")
     pets = cursor.fetchall()
     connection.close()
     return pets
+
+@app.route('/create_pets', methods=['POST'])
+def create_pet():
+    try:
+        new_pet = request.json
+        connection = get_db_connection()
+        cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cursor.execute("INSERT INTO pets (name, age, breed) VALUES (%s, %s, %s) RETURNING *", (new_pet['name'], new_pet['age'], new_pet['breed']))
+        created_pet = cursor.fetchone()  
+        connection.commit()
+        return created_pet, 201
+    except Exception as e:
+        return str(e), 500
+    
+
 
 app.run()
